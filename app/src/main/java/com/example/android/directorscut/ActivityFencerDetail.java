49e29e738565273f73efcb7f66a393e965ca4af8
Masaya Takahashi <takahashi.masayar@gmail.com>
2019-11-08 14:22:56 -0800

    PoolSetup & Fencer Settings
    Added & Updated UI for managing fencer setttings.
    
    Fxn not implemented

---
+++ app/src/main/java/com/example/android/directorscut/ActivityFencerDetail.java
@@ -0,0 +1,13 @@
+package com.example.android.directorscut;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityFencerDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fencer_detail);
+    }
+}
