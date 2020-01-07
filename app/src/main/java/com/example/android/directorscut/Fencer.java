bce5f88d1ff9cb5e331603fa04ab9038f42735a0
Masaya Takahashi <takahashi.masayar@gmail.com>
2020-01-07 14:21:07 -0800

    Revert "Revert "Merge branch 'master' of https://github.com/MasOcelot/DirectorsCut""
    This reverts commit be0f2d27496dd2982b60e3de05f25782fded5ec8.

--- app/src/main/java/com/example/android/directorscut/Fencer.java
+++ app/src/main/java/com/example/android/directorscut/Fencer.java
@@ -1,12 +1,17 @@
 package com.example.android.directorscut;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.Comparator;
 
-public class Fencer implements Comparable<Fencer> {
+public class Fencer implements Comparable<Fencer>, Parcelable {
     private static int numFencers;
     private static int genericFencers = 1;
     private String lastName;
+    private String club;
     private int rating = 0;
+    private int ratingYear = 0;
     private int localIndex;
     private int vic;
     private int ts;
@@ -15,16 +20,49 @@ public class Fencer implements Comparable<Fencer> {
     private int place;
     private boolean leftHanded = false;
 
+    public Fencer(String name, int rating, int ratingYear) {
+        this(name);
+        this.setRating(rating);
+        this.setRatingYear(ratingYear);
+    }
+
     public Fencer(String name) {
         this.setLocalIndex(numFencers);
         numFencers++;
-        this.lastName = name;
+        this.setLastName(name);
+        this.setClub("Unattached");
     }
 
     public Fencer() {
-        this("JonDow_" + genericFencers);
+        this("Fencer-" + genericFencers);
         genericFencers++;
     }
+
+    protected Fencer(Parcel in) {
+        lastName = in.readString();
+        club = in.readString();
+        rating = in.readInt();
+        ratingYear = in.readInt();
+        localIndex = in.readInt();
+        vic = in.readInt();
+        ts = in.readInt();
+        tr = in.readInt();
+        ind = in.readInt();
+        place = in.readInt();
+        leftHanded = in.readByte() != 0;
+    }
+
+    public static final Creator<Fencer> CREATOR = new Creator<Fencer>() {
+        @Override
+        public Fencer createFromParcel(Parcel in) {
+            return new Fencer(in);
+        }
+
+        @Override
+        public Fencer[] newArray(int size) {
+            return new Fencer[size];
+        }
+    };
 
     @Override
     public String toString() {
@@ -70,6 +108,47 @@ public class Fencer implements Comparable<Fencer> {
         return rating;
     }
 
+    public int getRatingYear() {
+        return ratingYear;
+    }
+
+    public String getRatingString() {
+        String ratingStr = "";
+        String letter = "U";
+        switch (this.getRating()) {
+            case 1:
+                letter = "E";
+                break;
+            case 2:
+                letter = "D";
+                break;
+            case 3:
+                letter = "C";
+                break;
+            case 4:
+                letter = "B";
+                break;
+            case 5:
+                letter = "A";
+                break;
+            default:
+                letter = "U";
+        }
+        String yearString = "";
+        int year = this.getRatingYear();
+        if (year > 0) {
+            yearString = " " + year;
+        }
+        return ratingStr + letter + yearString;
+    }
+
+    public String getClub() {
+        if (this.club != null) {
+            return this.club;
+        }
+        return "";
+    }
+
     public boolean isLeftHanded() {
         return leftHanded;
     }
@@ -93,6 +172,14 @@ public class Fencer implements Comparable<Fencer> {
 
     public void setRating(int rating) {
         this.rating = rating;
+    }
+
+    public void setRatingYear(int year) {
+        this.ratingYear = year;
+    }
+
+    public void setClub(String club) {
+        this.club = club;
     }
 
     public void updateInd() {
@@ -168,5 +255,23 @@ public class Fencer implements Comparable<Fencer> {
     };
 
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
 
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(lastName);
+        parcel.writeString(club);
+        parcel.writeInt(rating);
+        parcel.writeInt(ratingYear);
+        parcel.writeInt(localIndex);
+        parcel.writeInt(vic);
+        parcel.writeInt(ts);
+        parcel.writeInt(tr);
+        parcel.writeInt(ind);
+        parcel.writeInt(place);
+        parcel.writeByte((byte) (leftHanded ? 1 : 0));
+    }
 }
