e892b3c8a5aa2313065602af7a1a34e310f33679
Masaya Takahashi <takahashi.masayar@gmail.com>
2020-01-22 12:06:36 -0800

    RecyclerView revisited initial commit

--- app/src/main/java/com/example/android/directorscut/ActivityPoolSetup.java
+++ app/src/main/java/com/example/android/directorscut/ActivityPoolSetup.java
@@ -3,6 +3,7 @@ package com.example.android.directorscut;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,6 +22,11 @@ public class ActivityPoolSetup extends AppCompatActivity implements View.OnClick
 
     private int numFencers = 5;
     private int scoreLimit = 0;
+
+    //RecyclerView Members
+    private RecyclerView mFencerRV;
+    private RecyclerView.Adapter mFRVAdapter;
+    private RecyclerView.LayoutManager mFRVLayoutManager;
 
     ArrayList<Fencer> psFencers = new ArrayList<Fencer>(){{
         add(new Fencer("Mas"));
